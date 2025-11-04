4cd430d6424f0d3d3fa389c348c65e4336e37ef9
José Enrique Menéndez Fernández <63372900+JoseEnriquw@users.noreply.github.com>
2025-11-04 14:55:41 -0300

    SceneManager and build setting

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Inicia la carga asíncrona de la escena "Nivel2" de forma aditiva
+        StartCoroutine(LoadSceneAsync("Hall_Principal"));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        // SceneMode.Additive añade la escena a las ya cargadas
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        // Espera hasta que la carga esté completa
+        while (!asyncLoad.isDone)
+        {
+            // Puedes usar asyncLoad.progress para una barra de carga
+            Debug.Log("Cargando escena: " + asyncLoad.progress * 100 + "%");
+            yield return null;
+        }
+
+        Debug.Log("Escena " + sceneName + " cargada de forma aditiva.");
+    }
+}
